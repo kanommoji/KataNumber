33d8c0ce27aead299a2d7657c9a525c045f728e2
sck shuhari <sckshuhari@scks-MacBook-Pro.local>
2019-08-13 10:40:34 +0700

    EDIT : สามารถเก็บค่าใส่ array และแสดงผลเป็น string ได้

--- kata.go
+++ kata.go
@@ -1,5 +1,59 @@
 package kata
 
+import "fmt"
+
 func Kata(number int) string {
-	return "|\n|"
+	var lcdNumber [8]int
+	var kataNumber string
+	if number == 1 {
+		lcdNumber[4] = 1
+		lcdNumber[7] = 1
+	}
+	if number == 2 {
+		lcdNumber[1] = 1
+		lcdNumber[3] = 1
+		lcdNumber[4] = 1
+		lcdNumber[5] = 1
+		lcdNumber[6] = 1
+
+	}
+	if number == 3 {
+		lcdNumber[1] = 1
+		lcdNumber[3] = 1
+		lcdNumber[4] = 1
+		lcdNumber[6] = 1
+		lcdNumber[7] = 1
+
+	}
+
+	if lcdNumber[1] == 1 {
+		kataNumber = fmt.Sprint(kataNumber, "_\n")
+		lcdNumber[1] = 0
+	}
+	if lcdNumber[3] == 1 && lcdNumber[4] == 1 {
+		kataNumber = fmt.Sprint(kataNumber, "_|\n")
+		lcdNumber[3] = 0
+		lcdNumber[4] = 0
+	}
+	if lcdNumber[5] == 1 && lcdNumber[6] == 1 {
+		kataNumber = fmt.Sprint(kataNumber, "|_\n")
+		lcdNumber[5] = 0
+		lcdNumber[6] = 0
+	}
+	if lcdNumber[6] == 1 && lcdNumber[7] == 1 {
+		kataNumber = fmt.Sprint(kataNumber, "_|\n")
+		lcdNumber[6] = 0
+		lcdNumber[7] = 0
+	}
+
+	if lcdNumber[4] == 1 {
+		kataNumber = fmt.Sprint(kataNumber, "|\n")
+		lcdNumber[4] = 0
+	}
+	if lcdNumber[7] == 1 {
+		kataNumber = fmt.Sprint(kataNumber, "|\n")
+		lcdNumber[7] = 0
+	}
+
+	return kataNumber
 }
--- kata_test.go
+++ kata_test.go
@@ -3,11 +3,31 @@ package kata
 import "testing"
 
 func Test_Kata_Input_1_Should_be_kata_1(t *testing.T) {
-	expectedResult := "|\n|"
+	expectedResult := "|\n|\n"
 
 	actualResult := Kata(1)
 
 	if expectedResult != actualResult {
-		t.Errorf("Expect %v but get %v", actualResult, expectedResult)
+		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
+	}
+}
+
+func Test_Kata_Input_2_Should_be_kata_2(t *testing.T) {
+	expectedResult := "_\n_|\n|_\n"
+
+	actualResult := Kata(2)
+
+	if expectedResult != actualResult {
+		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
+	}
+}
+
+func Test_Kata_Input_3_Should_be_kata_3(t *testing.T) {
+	expectedResult := "_\n_|\n_|\n"
+
+	actualResult := Kata(3)
+
+	if expectedResult != actualResult {
+		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
 	}
 }
