89a5022384a207a35b91409eefa7a8639ee2ea86
sck shuhari <sckshuhari@scks-MacBook-Pro.local>
2019-08-14 09:42:16 +0700

    EDIT : เปลี่ยนวิธีการแปลงเป็นเลขดิจิทัล

--- kata.go
+++ kata.go
@@ -3,44 +3,12 @@ package kata
 import "fmt"
 
 func ToDigitalNumbers(number int) string {
-	var lcdNumber [8]int
-	var kataNumber string
-	if number == 1 {
-		lcdNumber[4] = 1
-		lcdNumber[7] = 1
-	}
-	if number == 2 {
-		lcdNumber[1] = 1
-		lcdNumber[3] = 1
-		lcdNumber[4] = 1
-		lcdNumber[5] = 1
-		lcdNumber[6] = 1
-
-	}
-	if number == 3 {
-		lcdNumber[1] = 1
-		lcdNumber[3] = 1
-		lcdNumber[4] = 1
-		lcdNumber[6] = 1
-		lcdNumber[7] = 1
-
-	}
-
-	for i := 0; i < 8; i++ {
-		if lcdNumber[i] == 1 {
-			if i == 1 || i == 3 || i == 6 {
-				kataNumber = fmt.Sprint(kataNumber, "_")
-			} else {
-				kataNumber = fmt.Sprint(kataNumber, "|")
-			}
-			if i == 3 && lcdNumber[4] == 0 {
-				kataNumber = fmt.Sprint(kataNumber, "\n")
-			}
-			if i == 1 || i == 4 {
-				kataNumber = fmt.Sprint(kataNumber, "\n")
-			}
-		}
-	}
-
-	return kataNumber
+	Hight := []string{` `}
+	Meium := []string{` |`}
+	Low := []string{` |`}
+	var digitalNumber string
+	digitalNumber = fmt.Sprint(digitalNumber, Hight[0], "\n")
+	digitalNumber = fmt.Sprint(digitalNumber, Meium[0], "\n")
+	digitalNumber = fmt.Sprint(digitalNumber, Low[0])
+	return digitalNumber
 }
--- kata_test.go
+++ kata_test.go
@@ -3,29 +3,11 @@ package kata
 import "testing"
 
 func Test_Kata_Input_1_Should_be_kata_1(t *testing.T) {
-	expectedResult := "|\n|"
+	expectedResult := ` 
+ |
+ |`
 
 	actualResult := ToDigitalNumbers(1)
-
-	if expectedResult != actualResult {
-		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
-	}
-}
-
-func Test_Kata_Input_2_Should_be_kata_2(t *testing.T) {
-	expectedResult := "_\n_|\n|_"
-
-	actualResult := ToDigitalNumbers(2)
-
-	if expectedResult != actualResult {
-		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
-	}
-}
-
-func Test_Kata_Input_3_Should_be_kata_3(t *testing.T) {
-	expectedResult := "_\n_|\n_|"
-
-	actualResult := ToDigitalNumbers(3)
 
 	if expectedResult != actualResult {
 		t.Errorf("Expect %v but get %v", expectedResult, actualResult)
